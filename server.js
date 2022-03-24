c639383f29391859a001c5742852ff4cdfef7624
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-03-24 14:13:51 -0700

    Dates and times

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('/utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +10,7 @@ const sequelizeStore =require('connect-session-sequelize')(session.store);
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
